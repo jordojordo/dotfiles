89e940f1ac703d70289bb7b238f4fbed4d60caaa
jordojordo <jordonleach@gmail.com>
2025-04-08 05:47:06 -0400

    Update the prompt display to use a connector icon

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -74,7 +74,12 @@ battery_status() {
   fi
 }
 
-export PROMPT=$'\n$(battery_status)in $(directory_name) $(git_dirty)$(need_push)\n› '
+connector_icon=$'\u2570\U2500'  # ╰─
+
+# Line 1: Displays directory (and you can add Git info, battery status, etc.)
+# Line 2: Uses the connector icon then the prompt.
+export PROMPT=$'\n%{$fg[cyan]%}╭─ %{$fg_bold[cyan]%}$(directory_name)%{$reset_color%}\n%{$fg[cyan]%}'"$connector_icon"' %{$reset_color%}'
+
 set_prompt () {
   export RPROMPT="%{$fg_bold[cyan]%}%{$reset_color%}"
 }
